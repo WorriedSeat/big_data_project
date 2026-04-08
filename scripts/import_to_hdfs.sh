b0aebed6f1ff55034c26329dea4284f229b828fe
WorriedSeat <vanish_for.white@mail.ru>
2026-04-08 20:10:07 +0300

    adding script for loading into hdfs

---
+++ scripts/import_to_hdfs.sh
@@ -0,0 +1,26 @@
+password=$(cat secrets/.psql.pass | tr -d '\n\r')
+
+hdfs dfs -rm -r -f /user/team14/project/warehouse || true
+
+echo "Loading data in hdfs (Parquet + Snappy)..."
+
+sqoop import-all-tables \
+    --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team14_projectdb \
+    --username team14 \
+    --password "$password" \
+    --warehouse-dir /user/team14/project/warehouse \
+    --as-parquetfile \
+    --compression-codec=snappy \
+    --m 1
+
+echo "Loaded everything on HDFS"
+
+echo ""
+echo "Results in HDFS:"
+hdfs dfs -ls /user/team14/project/warehouse
+
+echo ""
+echo "Tables:"
+hdfs dfs -ls /user/team14/project/warehouse/airlines
+hdfs dfs -ls /user/team14/project/warehouse/airports
+hdfs dfs -ls /user/team14/project/warehouse/flights
